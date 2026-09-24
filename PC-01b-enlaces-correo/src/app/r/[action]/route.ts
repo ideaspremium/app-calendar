2c81850910cfbd32835a12b7af145e78d921d9d2
ideaspremium <produccion@ideaspremium.com>
2026-09-24 14:39:06 -0400

    Quitar copias sobrantes del zip PC-01b

--- PC-01b-enlaces-correo/src/app/r/[action]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-import { redirectForRef } from "@/lib/booking-ref-redirect";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(req: NextRequest, { params }: { params: Promise<{ action: string }> }) {
-  const { action } = await params;
-  return redirectForRef(req, action, req.nextUrl.searchParams.get("ref"));
-}
